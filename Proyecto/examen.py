d90c88aa3e4e6324c35ba50247370bfeaf0c2ee5
unknown <achavezgarcia.fi.013@gmail.com>
2022-04-16 14:05:15 -0500

    Mejore el como se muestran las respuestas y añadi que se muestre el tiempo en el que se resolvio el examen

--- Proyecto/examen.py
+++ Proyecto/examen.py
@@ -1,9 +1,12 @@
 """
 El programa mostrará el examen de opción multiple
 """
+import time
 
 class examen:    
     def opcionMultiple(self,nombre,asignatura,numero):
+        respuestas = []
+        tiempo = []
         finalizado1 = 0
         finalizado2 = 0
         indice1 = 0
@@ -24,12 +27,17 @@ class examen:
               indice2+=1
               salida = preguntas[indice1][indice2]
               if(isinstance(salida,int)):
+                  inicio=time.time()
                   respuesta=int(input('Seleccion: '))
                   if(salida == respuesta):
-                      print('\nRespuesta correcta\n')
+                      fin=time.time()
+                      tiempo.append(str(int(fin-inicio)))
+                      respuestas.append(str(indice1+1)+'. Respuesta correcta')
                       finalizado2 = 1
                   else:
-                      print('\nRespuesta incorrecta\n')
+                      fin=time.time()
+                      tiempo.append(str(int(fin-inicio)))
+                      respuestas.append(str(indice1+1)+'. Respuesta incorrecta')
                       finalizado2 = 1
               else:
                   print('\n'+preguntas[indice1][indice2])
@@ -38,5 +46,15 @@ class examen:
             indice2 = 0
             if(len(preguntas) == indice1):
                 finalizado1=1
+        return respuestas,tiempo
         #examenFinal.close() 
-            
+        
+    """
+    La funcion solucion va a mostrar el resultado que obtuvo el alumno en el examen,
+    ademas de que va a mostrar el tiempo que se tomo cada persona al resolver cada
+    pregunta
+    """
+    def solucion(self,respuestas,tiempo):
+        print('\nEl resultado del examen es el siguiente: \n')
+        for i in range(len(respuestas)):
+            print(respuestas[i]+'. Tiempo: '+tiempo[i]+' segundos\n')
--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,34 +1,50 @@
 # -*- coding: cp1252 -*-
 """
-Proyecto. Examenes de opción multiple
+Proyecto. Examenes de opcion multiple
 """
 
 import examen as ex
+import time
 
 class principal:    
     def main(self):
+        respuestas = []
+        tiempo = []
         nombre = ''
         asignatura = ''
         numero = ''
         salida = 0
-        print('\t\t\tElaborador de ex�menes.')
+        print('\t\t\tElaborador de examenes.')
         while(salida == 0):
-            print('\t\t\t\nMenu\n1. Crear examen\n2. Realizar examen\n3. Salir\n')
-            opcion = int(input('Selecciona una opci�n: '))
+            print('\t\t\t\nMenu\n1. Crear examen\n2. Realizar examen\n3. Ressultados\n4. Salir\n')
+            opcion = int(input('Selecciona una opcion: '))
             
             if(opcion == 1):
                 nombre = input('Nombre del examen: ')
                 asignatura = input('Nombre de la asignatura: ')
                 numero = int(input('Numero total de preguntas: '))
             elif(opcion == 2):
+            #En esta opcion se debe de crear primero un examen, de lo contrario se
+            #mostrara un mensaje al usuario diciendole esto. Además, se va a mostrar
+            #que se tomo para realizar el examen; el resultado estara en segundos.
                 if(nombre == '' or asignatura == '' or numero == ''):
-                    print('\nXD\n')
+                    print('\nPrimero debes de crear un examen para poder acceder\n')
                 else:
-                    ex.examen().opcionMultiple(nombre,asignatura,numero)
+                    inicio=time.time()
+                    respuestas,tiempo = ex.examen().opcionMultiple(nombre,asignatura,numero)
+                    fin=time.time()
+                    print('\nTiempo en que se resolvio el examen: '+str(int(fin-inicio))+' segundos')
             elif(opcion == 3):
+            #Esta opcion va a mostrar el resultado del examen. En caso de que no se
+            #tenga ninguna respuesta se va a mandar un mensaje al usuario
+                if(len(respuestas) == 0):
+                    print('\nNo hay ninguna respuesta\n')
+                else:
+                    ex.examen().solucion(respuestas,tiempo)
+            elif(opcion == 4):
                 salida = 1
             else:
-                print('\nOpci�n no valida\n')
+                print('\nOpcion no valida\n')
         print('\nSaliendo del programa')
 
 prin = principal()
